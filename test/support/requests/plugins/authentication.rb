77dce28b741db6ee242ad6695a82fe3759c4db4e
HoneyryderChuck <cardoso_tiago@hotmail.com>
2023-06-13 11:42:52 +0100

    add tests for -sess digest auth

--- test/support/requests/plugins/authentication.rb
+++ test/support/requests/plugins/authentication.rb
@@ -44,6 +44,20 @@ module Requests
         end
       end
 
+      %w[MD5 SHA1].each do |alg|
+        define_method "test_plugin_digest_authentication_#{alg}_sess" do
+          start_test_servlet(DigestServer, algorithm: "#{alg}-sess") do |server|
+            uri = "#{server.origin}/"
+            session = HTTPX.plugin(:digest_authentication).with_headers("cookie" => "fake=fake_value")
+            response = session.digest_auth(user, server.get_passwd(user)).get(uri)
+            verify_status(response, 200)
+            body = json_body(response)
+            verify_header(body, "authenticated", true)
+            verify_header(body, "user", user)
+          end
+        end
+      end
+
       def test_plugin_digest_authentication_bypass
         session = HTTPX.plugin(:digest_authentication).with_headers("cookie" => "fake=fake_value")
         response = session.get(digest_auth_uri)
